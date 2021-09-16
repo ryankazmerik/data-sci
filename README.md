79b77eaea9bc0a3b672359b914bd52af3e6a9804
Ryan Kazmerik <rkazmerik@stellaralgo.com>
2021-09-15 22:44:29 -0600

    Reorganized retention v2 project structure

--- README.md
+++ README.md
@@ -5,4 +5,5 @@ You know, for data science.
 * Remove customerID from the model (skewed feature importance)
 * Set data types for all features (some are inferred wrong)
 
-Test commit from Datascience Admin
+## Questions
+* Are we scoring against the same dataset for all of a certain teams products? i.e. Are we training against > 2019 data for the Full Product scoring, and then doing that same training again for the Half Product? Seems like it could be a big improvement, to train once for all 4 products instead of one by one.
--- scripts/retention/v1/main.py
+++ scripts/retention/v1/main.py
@@ -7,99 +7,65 @@ import sys
 import xgboost as xgb
 import warnings
 
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import balanced_accuracy_score, roc_auc_score, make_scorer
-from sklearn.model_selection import GridSearchCV  # cross validation
-from sklearn.metrics import accuracy_score, confusion_matrix
-from sklearn.metrics import plot_confusion_matrix
-from sshtunnel import open_tunnel
 from pymongo import MongoClient
 
 if not sys.warnoptions:
     warnings.simplefilter("ignore")
 
+SERVER = '52.44.171.130' 
+DATABASE = 'datascience' 
+USERNAME = 'dsAdminRead' 
+PASSWORD = 'AbacAB459C_H&FsEkQ%wU='
+CNXN = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+SERVER+';DATABASE='+DATABASE+';UID='+USERNAME+';PWD='+ PASSWORD)
 
-def run_training(teamproductyear_id):
 
-    # connecting to Redshift
-    conn = psycopg2.connect(
-        dbname="dsprod",
-        host="sagemaker.cbpdnejrkweo.us-east-1.redshift.amazonaws.com",
-        port=5439,
-        user="admin",
-        password="QCxaxQrpijap79XX6TiX",
-        sslmode="require",
-    )
+def run_training(client_id, client_code, product, train_year, test_year):
 
-    cur2 = conn.cursor()
-    sample_query_1 = f"""select teamproductyearid,lkupclientid,clientcode,productgrouping,trainseasonyear,testseasonyear from ds.productyear_all r where teamproductyearid ={teamproductyear_id} ;"""
-    bnew = cur2.execute(sample_query_1)
-    pnew = cur2.fetchall()
-    dfparam = pd.DataFrame(pnew)
-    new_columns_param = [
-        "teamproductyearid",
-        "lkupclientid",
-        "clientcode",
-        "productgrouping",
-        "trainseasonyear",
-        "testseasonyear",
-    ]
-    dfparam3 = pd.DataFrame(pnew, columns=new_columns_param)
-    dfparam3.head()
+    # GET DATA FROM MSSQL
+    cursor = CNXN.cursor()
 
-    paramindex = 0
+    client_id = 11 #bulls
+    product = "Full Season"
+    train_year = 2019
 
-    client_id = dfparam3._get_value(0, "lkupclientid")
-    client_code = dfparam3._get_value(0, "clientcode")
-    product_grouping = dfparam3._get_value(0, "productgrouping")
-    train_season_year = dfparam3._get_value(0, "trainseasonyear")
-    test_season_year = dfparam3._get_value(0, "testseasonyear")
+    query =  f"""
+        SELECT 
+            r.dimcustomermasterid,
+            recency,
+            attendancePercent,
+            totalSpent,
+            distToVenue,
+            source_tenure,
+            renewedBeforeDays,
+            missed_games_1,
+            missed_games_2,
+            missed_games_over_2,
+            isnextyear_buyer
+        FROM 
+            ds.retentionscoring r 
+        WHERE 
+            lkupclientid = {client_id} 
+        AND 
+            productgrouping = {"'"+ str(product) + "'"} 
+        AND 
+            year < {train_year};
+        """
+        
+    df = pd.read_sql(query, CNXN)
+        
+    CNXN.commit()
+    cursor.close()
 
-    print(
-        "\n\n",
-        client_id,
-        client_code,
-        product_grouping,
-        train_season_year,
-        test_season_year,
-        end="\n\n\n",
-    )
 
-    cur = conn.cursor()
-    sample_query = f"""select r.dimcustomermasterid,recency,attendancePercent,totalSpent,distToVenue,source_tenure,renewedBeforeDays,missed_games_1,missed_games_2,missed_games_over_2,isnextyear_buyer,isnextyear_samepkg_buyer,pkgupgrade_status from ds.retentionscoring r where lkupclientid ={client_id} and productgrouping in({"'"+ str(product_grouping) + "'"}) and year<{train_season_year};"""
-    b = cur.execute(sample_query)
-    p = cur.fetchall()
-    df = pd.DataFrame(p)
-    new_columns = [
-        "dimcustomermasterid",
-        "recency",
-        "attendancePercent",
-        "totalSpent",
-        "distToVenue",
-        "source_tenure",
-        "renewedBeforeDays",
-        "missed_games_1",
-        "missed_games_2",
-        "missed_games_over_2",
-        "isnextyear_buyer",
-        "isnextyear_samepkg_buyer",
-        "pkgupgrade_status",
-    ]
-    df3 = pd.DataFrame(p, columns=new_columns)
-    df3.drop(["isnextyear_samepkg_buyer", "pkgupgrade_status"], axis=1, inplace=True)
-    df3.head()
+    df["dimcustomermasterid"] = pd.to_numeric(df["dimcustomermasterid"])
+    df["attendancePercent"] = pd.to_numeric(df["attendancePercent"])
+    df["totalSpent"] = pd.to_numeric(df["totalSpent"])
+    df["distToVenue"] = pd.to_numeric(df["distToVenue"])
 
-    df3.count()
-
-    df3["dimcustomermasterid"] = pd.to_numeric(df3["dimcustomermasterid"])
-    df3["attendancePercent"] = pd.to_numeric(df3["attendancePercent"])
-    df3["totalSpent"] = pd.to_numeric(df3["totalSpent"])
-    df3["distToVenue"] = pd.to_numeric(df3["distToVenue"])
-
-    X = df3.drop(["isnextyear_buyer"], axis=1).copy()
+    X = df.drop(["isnextyear_buyer"], axis=1).copy()
     X.head()
 
-    y = df3["isnextyear_buyer"].copy()
+    y = df["isnextyear_buyer"].copy()
     y.head()
 
     y.unique()
@@ -135,7 +101,7 @@ def run_training(teamproductyear_id):
     feature_importances_df
 
     feature_importances = feature_importances_df[["feature", "importance"]]
-    feature_importances["productgrouping"] = product_grouping
+    feature_importances["product"] = product
     feature_importances = feature_importances[["feature", "importance"]]
     feature_importances.drop([0], axis=0, inplace=True)
     feature_importances
@@ -156,6 +122,7 @@ def run_training(teamproductyear_id):
     myquery = {"_id": client_code}
     tenant_doc = collection.find_one(myquery)
     today = datetime.datetime.now()
+    
     if "date_last_retention_scores" not in tenant_doc:
         tenant_doc["date_last_retention_scores"] = {}
     tenant_doc["date_last_retention_scores"] = today
@@ -163,12 +130,20 @@ def run_training(teamproductyear_id):
 
     if "attributes_std" not in tenant_doc:
         tenant_doc["attributes_std"] = {}
-    tenant_doc["attributes_std"][product_grouping] = feature_importance_dict
+    tenant_doc["attributes_std"][product] = feature_importance_dict
     collection.update_one(myquery, {"$set": tenant_doc}, upsert=True)
+    
+    # connecting to Redshift
+    conn=psycopg2.connect(dbname = 'dsprod',
+        host = 'sagemaker.cbpdnejrkweo.us-east-1.redshift.amazonaws.com',
+        port = 5439,
+        user = 'admin',
+        password='QCxaxQrpijap79XX6TiX',
+        sslmode='require')
 
     # Importing the data
     cur = conn.cursor()
-    sample_query2 = f"""select r.dimcustomermasterid,recency,attendancePercent,totalSpent,distToVenue,source_tenure,renewedBeforeDays,missed_games_1,missed_games_2,missed_games_over_2,isnextyear_buyer,isnextyear_samepkg_buyer,pkgupgrade_status from ds.retentionscoring r where lkupclientid ={client_id} and productgrouping in({"'"+ str(product_grouping) + "'"}) and year={test_season_year};"""
+    sample_query2 = f"""select r.dimcustomermasterid,recency,attendancePercent,totalSpent,distToVenue,source_tenure,renewedBeforeDays,missed_games_1,missed_games_2,missed_games_over_2,isnextyear_buyer,isnextyear_samepkg_buyer,pkgupgrade_status from ds.retentionscoring r where lkupclientid ={client_id} and productgrouping in({"'"+ str(product) + "'"}) and year={test_year};"""
     b2 = cur.execute(sample_query2)
     p2 = cur.fetchall()
     df_test2 = pd.DataFrame(p2)
@@ -187,6 +162,9 @@ def run_training(teamproductyear_id):
         "isnextyear_samepkg_buyer",
         "pkgupgrade_status",
     ]
+
+    new_columns = ['dimcustomermasterid','recency','attendancePercent','totalSpent','distToVenue','source_tenure','renewedBeforeDays','missed_games_1','missed_games_2','missed_games_over_2','isnextyear_buyer','isnextyear_samepkg_buyer','pkgupgrade_status']
+
     df_test = pd.DataFrame(p2, columns=new_columns)
     df_test.drop(
         ["isnextyear_samepkg_buyer", "pkgupgrade_status"], axis=1, inplace=True
@@ -227,9 +205,9 @@ def run_training(teamproductyear_id):
 
     newscors = result_test[["dimcustomermasterid", "buyer"]]
     newscors.columns = ["dimcustomermasterid", "buyer_score"]
-    newscors["year"] = test_season_year
+    newscors["year"] = test_year
     newscors["lkupclientid"] = client_id
-    newscors["productgrouping"] = product_grouping
+    newscors["productgrouping"] = product
     newscors["insertDate"] = date_time
     # newscors
 
@@ -277,8 +255,8 @@ def run_training(teamproductyear_id):
         ["dimcustomermasterid", "buyer_score"]
     ]  # dimcustomermasterid
     mongoscores["id_tenant"] = client_code
-    mongoscores["productgrouping"] = product_grouping
-    mongoscores["year"] = test_season_year
+    mongoscores["productgrouping"] = product
+    mongoscores["year"] = test_year
     mongoscores["insertDate"] = datetime.datetime.now()
     mongoscores.columns = [
         "customerNumber",
@@ -289,12 +267,10 @@ def run_training(teamproductyear_id):
         "date",
     ]
     mongoscores_dict = mongoscores.to_dict(orient="records")
-    # mongoscores_dict
-    # mongoscores.to_numpy()
 
     feature_importancesdict = feature_importances.to_dict(orient="records")
     feature_importancesdict
-    # aa = {feature_importancesdict['feature']: feature_importancesdict['importance']}
+
     aa = [
         {sample_dict["feature"]: sample_dict["importance"]}
         for sample_dict in feature_importancesdict
@@ -375,7 +351,7 @@ def run_training(teamproductyear_id):
     feature_importances2["modelVersnNumber"] = 2
     feature_importances2["scoreDate"] = date_time
     feature_importances2["loadId"] = 0
-    feature_importances2["product"] = product_grouping
+    feature_importances2["product"] = product
     feature_importances2.columns = [
         "attribute",
         "indexValue",
@@ -414,7 +390,7 @@ def run_training(teamproductyear_id):
             + "'"
             + ","
             + "'"
-            + str(product_grouping)
+            + str(product)
             + "'"
             + ","
             + str(round(row.indexValue, 4))
@@ -432,7 +408,7 @@ def run_training(teamproductyear_id):
             + str(row.loadId)
             + ")"
         )
-        # cursor.execute("INSERT INTO stlrMLS.dw.lkupRetentionAttributeImportance (attribute,product,indexValue,rank,lkupClientId,modelVersnNumber,scoreDate,loadId) values(" + "'" +str(row.attribute)+"'"+","+ "'"+str(product_grouping)+"'" +"," + str(round(row.indexValue,4)) + "," + str(row.attrank)+ ","+ str(row.lkupClientId) + "," + str(row.modelVersnNumber)+ "," +str(row.scoreDate) + ","+ str(row.loadId)  + ")")
+
     cnxn.commit()
     cursor.close()
 
@@ -440,16 +416,28 @@ def run_training(teamproductyear_id):
 # teamproductyear_id = 32  # 1,2...74
 # 18,19,20, 24, 25, 26 no data for testing
 
+#def get_params(teamproductyearid):
+
+
 if __name__ == "__main__":
 
-    team_set = [71, 72, 73, 74]
+    teams = [17]
 
-    for id in team_set:
+    for teamproductyearid in teams:
+        
+        #params = get_params(teamproductyearid)
 
-        try:
-            run_training(id)
-            print("RETENTION SCORING COMPLETED FOR:", id, end="\n\n")
+        #run_training(17, "rainiers", "Full Season", 2019, 2021)
+        run_training(93, "panthers", "Full Season", 2019, 2021)
+        
+        # team_set = [1]
 
-        except Exception as err:
-            print(err)
-            print("RETENTION SCORING FAILED FOR:", id, end="\n\n")
+    # for id in team_set:
+
+    #     try:
+    #         run_training(id)
+    #         print("RETENTION SCORING COMPLETED FOR:", id, end="\n\n")
+
+    #     except Exception as err:
+    #         print(err)
+    #         print("RETENTION SCORING FAILED FOR:", id, end="\n\n")
