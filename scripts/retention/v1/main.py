5c9dccc03c5b4e9e1379e0e3571e3540e1fb55bf
Ryan Kazmerik <rkazmerik@stellaralgo.com>
2021-09-29 19:03:59 -0600

    Consolidated getting datasets

--- scripts/retention/v1/main.py
+++ scripts/retention/v1/main.py
@@ -21,29 +21,11 @@ response = ssm_client.get_parameter(
     Name="/product/ai/notebook/db-connections/LEGACY-MSSQL-QA-VPC-WRITE",
     WithDecryption=True,
 )["Parameter"]["Value"]
-conn = json.loads(response)
+CONN = json.loads(response)
 
-#override SSM server IP to public IP
-conn['server'] = "52.44.171.130"
+# override SSM server IP to public IP
+CONN["server"] = "52.44.171.130"
 
-print(conn)
-
-CNXN = pyodbc.connect(
-    "DRIVER={ODBC Driver 17 for SQL Server}"
-    + ";SERVER="
-    + conn["server"]
-    + ";DATABASE="
-    + conn["database"]
-    + ";UID="
-    + conn["username"]
-    + ";PWD="
-    + conn["password"]
-)
-
-# NOTE: SSM Parameter timing out on MSSQL connection : need to use internal IP??
-# NOTE: dsAdminWrite can't read params
-# NOTE: get df_train & df_test from single data pull
-# NOTE: consolidate SQL cnxn's - only use dsAdminWrite
 # NOTE: refactor feature_importances2 code
 # NOTE: reformat insert statements
 # NOTE: add inline documentation
@@ -52,6 +34,18 @@ CNXN = pyodbc.connect(
 
 
 def get_params(teamproductyear_id):
+
+    CNXN = pyodbc.connect(
+        "DRIVER={ODBC Driver 17 for SQL Server}"
+        + ";SERVER="
+        + CONN["server"]
+        + ";DATABASE="
+        + CONN["database"]
+        + ";UID="
+        + CONN["username"]
+        + ";PWD="
+        + CONN["password"]
+    )
 
     # query sql for parameters
     cursor = CNXN.cursor()
@@ -78,42 +72,35 @@ def get_params(teamproductyear_id):
     CNXN.commit()
     cursor.close()
 
-    print(df_params)
-    print()
-
     return df_params
 
 
 def get_datasets(df_params):
 
-    cust_database = df_params["stlrDBName"]
-
-    cnxn = pyodbc.connect(
-        "DRIVER={ODBC Driver 17 for SQL Server};SERVER="
-        + SERVER
+    CNXN = pyodbc.connect(
+        "DRIVER={ODBC Driver 17 for SQL Server}"
+        + ";SERVER="
+        + CONN["server"]
         + ";DATABASE="
-        + cust_database
+        + df_params["stlrDBName"]
         + ";UID="
-        + USERNAME
+        + CONN["username"]
         + ";PWD="
-        + PASSWORD
+        + CONN["password"]
     )
-    cursor2 = cnxn2.cursor()
-    # Prepare the stored procedure execution script and parameter values
-    storedProc = f"""Exec [ds].[getRetentionScoringModelData] {client_id}"""
-    params = client_id
-    cnxn.commit()
-    cursor2.close()
 
-    dftrain = pd.read_sql(storedProc, cnxn2)
+    cursor = CNXN.cursor()
+    storedProc = (
+        f"""Exec [ds].[getRetentionScoringModelData] {df_params['lkupclientid']}"""
+    )
 
-    dfproduct = dftrain[dftrain["productGrouping"] == product_grouping]
+    df = pd.read_sql(storedProc, CNXN)
 
-    dfproduct["year"] = pd.to_numeric(dfproduct["year"])
+    CNXN.commit()
+    cursor.close()
 
-    dfyear = dfproduct[dfproduct["year"] < train_season_year]
-
-    df3 = dfyear[
+    # select features from df
+    df = df[
         [
             "dimCustomerMasterId",
             "recency",
@@ -126,64 +113,42 @@ def get_datasets(df_params):
             "missed_games_2",
             "missed_games_over_2",
             "isNextYear_Buyer",
+            "year",
+            "productGrouping",
         ]
     ]
 
-    df3.head()
-
-    df3["dimCustomerMasterId"] = pd.to_numeric(df3["dimCustomerMasterId"])
-    df3["attendancePercent"] = pd.to_numeric(df3["attendancePercent"])
-    df3["totalSpent"] = pd.to_numeric(df3["totalSpent"])
-    df3["distToVenue"] = pd.to_numeric(df3["distToVenue"])
-
-    X = df3.drop(["isNextYear_Buyer"], axis=1).copy()
-    y = df3["isNextYear_Buyer"].copy()
-
-    return df_train, df_test, df_target
-
-
-def get_train_dataset(df_params):
-
-    # GET DATA FROM MSSQL
-    cursor = CNXN.cursor()
-
-    query = f"""
-        SELECT 
-            r.dimcustomermasterid,
-            recency,
-            attendancePercent,
-            totalSpent,
-            distToVenue,
-            source_tenure,
-            renewedBeforeDays,
-            missed_games_1,
-            missed_games_2,
-            missed_games_over_2,
-            isnextyear_buyer
-        FROM 
-            ds.retentionscoring r 
-        WHERE 
-            lkupclientid = {df_params['lkupclientid']} 
-        AND 
-            productgrouping = {"'"+ df_params['productgrouping'] + "'"} 
-        AND 
-            year < {df_params["trainseasonyear"]};
-        """
-
-    df = pd.read_sql(query, CNXN)
-
-    CNXN.commit()
-    cursor.close()
-
-    df["dimcustomermasterid"] = pd.to_numeric(df["dimcustomermasterid"])
+    # apply some data type transformations
+    df["year"] = pd.to_numeric(df["year"])
+    df["dimCustomerMasterId"] = pd.to_numeric(df["dimCustomerMasterId"])
     df["attendancePercent"] = pd.to_numeric(df["attendancePercent"])
     df["totalSpent"] = pd.to_numeric(df["totalSpent"])
     df["distToVenue"] = pd.to_numeric(df["distToVenue"])
 
-    df_train = df.drop(["isnextyear_buyer"], axis=1).copy()
-    df_target = df["isnextyear_buyer"].copy()
+    # create df_train filtered by product and train year
+    df_train = df[
+        (df["productGrouping"] == df_params["productgrouping"])
+        & (df["year"] < df_params["trainseasonyear"])
+    ]
 
-    return df_train, df_test, df_target
+     # create df_target filtered by product and train year
+    df_target = df_train["isNextYear_Buyer"].copy()
+
+    # drop columns from df_train not needed for training
+    df_train = df_train.drop(
+        ["isNextYear_Buyer", "productGrouping", "year"], axis=1
+    )
+   
+    # create df_test filtered by product and test year
+    df_test = df[
+        (df["productGrouping"] == df_params["productgrouping"])
+        & (df["year"] >= df_params["testseasonyear"])
+    ]
+
+    # drop columns from df_test not needed for testing
+    df_test = df_test.drop(["isNextYear_Buyer", "productGrouping", "year"], axis=1).copy()
+
+    return df_train, df_target, df_test
 
 
 def create_model(df_train, df_target):
@@ -253,47 +218,6 @@ def create_model(df_train, df_target):
     return clf, feature_importances2
 
 
-def get_test_dataset(df_params):
-
-    cursor = CNXN.cursor()
-
-    query = f"""
-        SELECT 
-            r.dimcustomermasterid,
-            recency,
-            attendancePercent,
-            totalSpent,
-            distToVenue,
-            source_tenure,
-            renewedBeforeDays,
-            missed_games_1,
-            missed_games_2,
-            missed_games_over_2,
-            isnextyear_buyer
-        FROM 
-            ds.retentionscoring r 
-        WHERE 
-            lkupclientid ={df_params['lkupclientid']} 
-        AND 
-            productgrouping in({"'"+ str(df_params['productgrouping']) + "'"}) 
-        AND 
-            year={df_params['testseasonyear']} ;"""
-
-    df_test = pd.read_sql(query, CNXN)
-
-    CNXN.commit()
-    cursor.close()
-
-    df_test["dimcustomermasterid"] = pd.to_numeric(df_test["dimcustomermasterid"])
-    df_test["attendancePercent"] = pd.to_numeric(df_test["attendancePercent"])
-    df_test["totalSpent"] = pd.to_numeric(df_test["totalSpent"])
-    df_test["distToVenue"] = pd.to_numeric(df_test["distToVenue"])
-
-    df_test = df_test.drop(["isnextyear_buyer"], axis=1).copy()
-
-    return df_test
-
-
 def calc_retention_scores(df_params, df_test, clf):
 
     # make predictions for test data
@@ -307,14 +231,13 @@ def calc_retention_scores(df_params, df_test, clf):
     df_y_pred_test.columns = ["nonbuyer", "buyer"]
 
     result_test = pd.concat([df_y_pred_test, df_test], axis=1, join="inner")
-    # result_test
 
     result_test = result_test.drop(["nonbuyer"], axis=1).copy()
 
     result_test["buyer"] = pd.to_numeric(result_test["buyer"])
 
-    newscors = result_test[["dimcustomermasterid", "buyer"]]
-    newscors.columns = ["dimcustomermasterid", "buyer_score"]
+    newscors = result_test[["dimCustomerMasterId", "buyer"]]
+    newscors.columns = ["dimCustomerMasterId", "buyer_score"]
     newscors["year"] = df_params["testseasonyear"]
     newscors["lkupclientid"] = df_params["lkupclientid"]
     newscors["productgrouping"] = df_params["productgrouping"]
@@ -325,22 +248,19 @@ def calc_retention_scores(df_params, df_test, clf):
 
 def write_retention_scores(df_params, retention_scores):
 
-    server = "52.44.171.130"
-    database = "datascience"
-    username = "nrad"
-    password = "ThisIsQA123"
-    cnxn = pyodbc.connect(
-        "DRIVER={ODBC Driver 17 for SQL Server};SERVER="
-        + server
+    CNXN = pyodbc.connect(
+        "DRIVER={ODBC Driver 17 for SQL Server}"
+        + ";SERVER="
+        + CONN["server"]
         + ";DATABASE="
-        + database
+        + CONN["database"]
         + ";UID="
-        + username
+        + CONN["username"]
         + ";PWD="
-        + password
+        + CONN["password"]
     )
 
-    cursor = cnxn.cursor()
+    cursor = CNXN.cursor()
 
     # Insert Dataframe into SQL Server:
     for index, row in retention_scores.iterrows():
@@ -370,24 +290,22 @@ def write_retention_scores(df_params, retention_scores):
 
 def write_feature_importances(df_params, feature_importances):
 
-    server = "52.44.171.130"
-    database = "datascience"
-    username = "nrad"
-    password = "ThisIsQA123"
-    cnxn = pyodbc.connect(
-        "DRIVER={ODBC Driver 17 for SQL Server};SERVER="
-        + server
+    CNXN = pyodbc.connect(
+        "DRIVER={ODBC Driver 17 for SQL Server}"
+        + ";SERVER="
+        + CONN["server"]
         + ";DATABASE="
-        + database
+        + CONN["database"]
         + ";UID="
-        + username
+        + CONN["username"]
         + ";PWD="
-        + password
+        + CONN["password"]
     )
 
-    cursor = cnxn.cursor()
+    cursor = CNXN.cursor()
 
     for index, row in feature_importances.iterrows():
+        
         cursor.execute(
             "INSERT INTO stlrMILB.dw.lkupRetentionAttributeImportance (attribute,product,indexValue,rank,lkupClientId,modelVersnNumber,scoreDate,loadId) values("
             + "'"
@@ -414,37 +332,38 @@ def write_feature_importances(df_params, feature_importances):
             + ")"
         )
 
-    cnxn.commit()
+    CNXN.commit()
     cursor.close()
 
 
 if __name__ == "__main__":
 
     teams = [
+        {"clientcode": "tester", "teamproductyear_ids": [7]},
         # {"clientcode": "66ers", "teamproductyear_ids": [18, 19, 20]},
-        {"clientcode": "bulls", "teamproductyear_ids": [4, 5, 6, 7]},
-        {"clientcode": "drive", "teamproductyear_ids": [63, 64, 65, 66]},
-        {"clientcode": "elpaso", "teamproductyear_ids": [41, 42, 43]},
-        # {"clientcode": "fireflies", "teamproductyear_ids": [38, 39, 40]},
-        {"clientcode": "grizzlies", "teamproductyear_ids": [34, 35, 36, 37]},
-        {"clientcode": "hartfordyardgoats", "teamproductyear_ids": [53, 54]},
-        # {"clientcode": "hops", "teamproductyear_ids": [1, 2, 3, 96]},
-        # {"clientcode": "indyindians", "teamproductyear_ids": [24, 25, 26]},
-        {"clientcode": "kanecounty", "teamproductyear_ids": [71, 72]},
-        {"clientcode": "knights", "teamproductyear_ids": [55, 56, 57]},
-        {"clientcode": "legends", "teamproductyear_ids": [33]},
-        {"clientcode": "loons", "teamproductyear_ids": [21, 22, 23]},
-        # {"clientcode": "okcdodgers", "teamproductyear_ids": [58, 59, 60, 61, 62]},
-        {"clientcode": "ports", "teamproductyear_ids": [30, 31, 32]},
-        {"clientcode": "rainiers", "teamproductyear_ids": [15, 16, 17]},
-        {"clientcode": "rattlers", "teamproductyear_ids": [27, 28, 29]},
-        {"clientcode": "renoaces", "teamproductyear_ids": [47, 48, 49, 50]},
-        {"clientcode": "rivercats", "teamproductyear_ids": [8, 9, 10, 11]},
-        {"clientcode": "rrexpress", "teamproductyear_ids": [44, 45, 46]},
-        {"clientcode": "stormchasers", "teamproductyear_ids": [51, 52]},
-        # {"clientcode": "stripers", "teamproductyear_ids": [67, 68, 69, 70]},
-        {"clientcode": "toledomudhens", "teamproductyear_ids": [73, 74]},
-        {"clientcode": "vegas51s", "teamproductyear_ids": [12, 13, 14]},
+        # {"clientcode": "bulls", "teamproductyear_ids": [4, 5, 6, 7]},
+        # {"clientcode": "drive", "teamproductyear_ids": [63, 64, 65, 66]},
+        # {"clientcode": "elpaso", "teamproductyear_ids": [41, 42, 43]},
+        # # {"clientcode": "fireflies", "teamproductyear_ids": [38, 39, 40]},
+        # {"clientcode": "grizzlies", "teamproductyear_ids": [34, 35, 36, 37]},
+        # {"clientcode": "hartfordyardgoats", "teamproductyear_ids": [53, 54]},
+        # # {"clientcode": "hops", "teamproductyear_ids": [1, 2, 3, 96]},
+        # # {"clientcode": "indyindians", "teamproductyear_ids": [24, 25, 26]},
+        # {"clientcode": "kanecounty", "teamproductyear_ids": [71, 72]},
+        # {"clientcode": "knights", "teamproductyear_ids": [55, 56, 57]},
+        # {"clientcode": "legends", "teamproductyear_ids": [33]},
+        # {"clientcode": "loons", "teamproductyear_ids": [21, 22, 23]},
+        # # {"clientcode": "okcdodgers", "teamproductyear_ids": [58, 59, 60, 61, 62]},
+        # {"clientcode": "ports", "teamproductyear_ids": [30, 31, 32]},
+        # {"clientcode": "rainiers", "teamproductyear_ids": [15, 16, 17]},
+        # {"clientcode": "rattlers", "teamproductyear_ids": [27, 28, 29]},
+        # {"clientcode": "renoaces", "teamproductyear_ids": [47, 48, 49, 50]},
+        # {"clientcode": "rivercats", "teamproductyear_ids": [8, 9, 10, 11]},
+        # {"clientcode": "rrexpress", "teamproductyear_ids": [44, 45, 46]},
+        # {"clientcode": "stormchasers", "teamproductyear_ids": [51, 52]},
+        # # {"clientcode": "stripers", "teamproductyear_ids": [67, 68, 69, 70]},
+        # {"clientcode": "toledomudhens", "teamproductyear_ids": [73, 74]},
+        # {"clientcode": "vegas51s", "teamproductyear_ids": [12, 13, 14]},
     ]
 
     for team in teams:
@@ -453,26 +372,19 @@ if __name__ == "__main__":
         for teamproductyear_id in teamproductyear_ids:
 
             df_params = get_params(teamproductyear_id)
-            # print("\n", df_params, end="\n\n")
+            print("\n", df_params, end="\n\n")
 
-            # df_train, df_test, df_target = get_datasets(df_params)
-            # print(df_train.head(), end="\n\n")
-            # print(df_test.head(), end="\n\n")
-            # print(df_target.head(), end="\n\n")
+            df_train, df_target, df_test = get_datasets(df_params)
 
-            # model, feature_importances = create_model(df_train, df_target)
-            # # print(model, end="\n\n")
-            # # print(feature_importances, end="\n\n")
+            model, feature_importances = create_model(df_train, df_target)
+            # print(model, end="\n\n")
+            # print(feature_importances, end="\n\n")
 
-            # df_test = get_test_dataset(df_params)
-            # # print(df_test, end="\n\n")
+            retention_scores = calc_retention_scores(df_params, df_test, model)
+            # print(retention_scores, end="\n\n")
 
-            # retention_scores = calc_retention_scores(df_params, df_test, model)
-            # # print(retention_scores, end="\n\n")
+            write_retention_scores(df_params, retention_scores)
 
-            # write_retention_scores(df_params, retention_scores)
+            write_feature_importances(df_params, feature_importances)
 
-            # write_feature_importances(df_params, feature_importances)
-            print()
-
-    # print("~ Fin ~", end="\n\n")
+    print("~ Fin ~", end="\n\n")
