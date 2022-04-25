a1c9b3f8335feb23d0f0783c0087af84fb4816c2
Ryan Kazmerik <rkazmerik@stellaralgo.com>
2022-04-25 12:45:32 -0600

    shared utilities working

--- shared_utilities/setup.py
+++ shared_utilities/setup.py
@@ -2,9 +2,9 @@ from setuptools import setup
 
 setup(
     name="shared_utilities",
-    version="0.1",
+    version="0.2",
     packages=["shared_utilities"],
     install_requires=[
-        "pycaret==2.3.5"
+        "boto3"
     ]
 )
--- shared_utilities/shared_utilities/helpers.py
+++ shared_utilities/shared_utilities/helpers.py
@@ -1,5 +1,33 @@
+import boto3
+import json
+import pyodbc
+
 def hello_world():
     
     msg = "Hello World"
     
     return msg
+
+
+def get_mssql_connection():
+
+    ssm_client = boto3.client("ssm", "us-east-1")
+    response = ssm_client.get_parameter(
+        Name=f"/customer/model-retention/ai/db-connections/data-sci-retention/database-write",
+        WithDecryption=True,
+    )["Parameter"]["Value"]
+    sql_connection = json.loads(response)
+
+    cnxn = pyodbc.connect(
+        "DRIVER={ODBC Driver 17 for SQL Server}"
+        + ";SERVER="
+        + sql_connection["server"]
+        + ";DATABASE="
+        + sql_connection["database"]
+        + ";UID="
+        + sql_connection["username"]
+        + ";PWD="
+        + sql_connection["password"]
+    )
+
+    return cnxn
