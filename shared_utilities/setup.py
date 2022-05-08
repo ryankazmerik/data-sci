6cbc377b1b3e4f45acaa47048c7e8736e6f9ee79
Ryan Kazmerik <rkazmerik@stellaralgo.com>
2022-05-08 00:14:41 -0600

    added functions to shared utils

--- shared_utilities/setup.py
+++ shared_utilities/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="shared_utilities",
-    version="0.2",
+    version="0.3",
     packages=["shared_utilities"],
     install_requires=[
         "boto3"
--- shared_utilities/shared_utilities/helpers.py
+++ shared_utilities/shared_utilities/helpers.py
@@ -1,3 +1,4 @@
+import awswrangler
 import boto3
 import json
 import pyodbc
@@ -9,25 +10,69 @@ def hello_world():
     return msg
 
 
-def get_mssql_connection():
+def get_mssql_connection(environment):
+
+    if environment == "QA":
+        env = "LEGACY-MSSQL-QA-VPC-WRITE"
+        serv = "52.44.171.130"
+    elif environment == "PRD":
+        env = "LEGACY-MSSQL-PROD-PRODUCT-WRITE"
+        serv = "34.206.73.189"
 
     ssm_client = boto3.client("ssm", "us-east-1")
     response = ssm_client.get_parameter(
-        Name=f"/customer/model-retention/ai/db-connections/data-sci-retention/database-write",
+        Name=f"/product/ai/notebook/db-connections/{env}",
         WithDecryption=True,
     )["Parameter"]["Value"]
     sql_connection = json.loads(response)
 
     cnxn = pyodbc.connect(
         "DRIVER={ODBC Driver 17 for SQL Server}"
-        + ";SERVER="
-        + sql_connection["server"]
-        + ";DATABASE="
-        + sql_connection["database"]
-        + ";UID="
-        + sql_connection["username"]
-        + ";PWD="
-        + sql_connection["password"]
+        + ";SERVER="+serv
+        + ";DATABASE="+sql_connection["database"]
+        + ";UID="+sql_connection["username"]
+        + ";PWD="+sql_connection["password"]
     )
 
     return cnxn
+
+
+def get_redshift_connection(environment):
+
+    # create connection to RedShift API
+
+    return "TODO: Implement this helper"
+
+
+def get_retention_model_dataset(environment, start_year, end_year):
+
+    # call SP to get full retention model dataset
+
+    # filter by start year and end year
+
+    return "TODO: Implement this helper"
+
+
+def get_event_propensity_model_dataset(environment, start_year, end_year):
+
+    # call SP to get full event propensity dataset
+
+    # filter by start year and end year
+
+    return "TODO: Implement this helper"
+
+
+def get_product_propensity_model_dataset(environment, start_year, end_year):
+
+    # call SP to get full product propensity dataset
+
+    # filter by start year and end year
+
+    return "TODO: Implement this helper"
+
+
+#if __name__ == "__main__":
+
+#    c = get_mssql_connection("PRD")
+
+#    print(c)
