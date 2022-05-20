1cff4eab943bf2bedf94e77cb843571177528807
Ryan Kazmerik <rkazmerik@stellaralgo.com>
2022-05-20 11:00:58 -0600

    Reviewed PTB experiment

--- procedures/datascience.sql
+++ procedures/datascience.sql
@@ -1,5 +1,7 @@
 SELECT * FROM datascience.ds.datadictionary
 
+
+
 INSERT INTO datascience.ds.datadictionary (
   feature, featureraw, datatype, source, engineered, description
 )
@@ -7,29 +9,4 @@ VALUES ('Gender Not Available','gender_not_available','object','Pycaret',True,'T
 
 
 
-
-CREATE OR REPLACE PROCEDURE ds.getRetentionDataset (
-  param IN integer, 
-  rs_out INOUT refcursor
-) AS $$
-
-BEGIN
-  OPEN rs_out FOR SELECT * FROM ds.dummytable where playerid = param;
-END;
-
-$$ LANGUAGE plpgsql;
-
-
-
-BEGIN;
-CALL ds.getRetentionDataset(11, 'mycursor');
-
-FETCH ALL FROM mycursor;
-COMMIT;
-
-
-BEGIN;
-CALL dw.getretentionscoringmodeldata(11, 2021, 2022, 'rkcursor');
-
-FETCH ALL FROM rkcursor;
-COMMIT;
+SELECT * FROM datascience.ds.teamsconfig
