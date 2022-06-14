6031ca0b3dea54d250fce625b0b138c0bd70e0b2
Ryan Kazmerik <rkazmerik@stellaralgo.com>
2022-06-14 17:20:55 -0600

    Tested new retention stored proc

--- procedures/product_propensity.sql
+++ procedures/product_propensity.sql
@@ -21,5 +21,5 @@ CALL stlrmilb.ds.getproductpropensitymodeldata(11, 2010, 2021, 'rkcursor');
 FETCH ALL FROM rkcursor;
 
 
-CALL stlrmls.ds.getproductpropensitymodeldata(6, 2021, 2022, 'temp_cursor');
+CALL stlrlagalaxy.ds.getproductpropensitymodeldata(6, 2021, 2022, 'temp_cursor');
 FETCH ALL FROM temp_cursor;
