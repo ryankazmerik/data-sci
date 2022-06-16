ff8b2e791e9e5e623b8c70b0609c0bdc47024f81
Ryan Kazmerik <rkazmerik@stellaralgo.com>
2022-06-16 13:36:41 -0600

    Testing new rs proc

--- procedures/retention.sql
+++ procedures/retention.sql
@@ -7,3 +7,6 @@ ORDER BY scoredate DESC
 
 CALL dw.getretentionmodeldata(11, 2010, 2021, 'temp_cursor');
 FETCH ALL FROM temp_cursor;
+
+CALL dw.getretentionmodeldata(6, 2020, 2022, 'temp_cursor');
+FETCH ALL FROM temp_cursor;
