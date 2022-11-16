8ee373e866aa0f2fdf65b96508ebe547d8d9fd56
PMorrison2 <pmorrison@stellaralgo.com>
2022-11-16 16:27:30 -0700

    Lambda - Update requirements

--- lambdas/dw_retention/src/pipeline.py
+++ lambdas/dw_retention/src/pipeline.py
@@ -1,4 +1,12 @@
+import boto3
+import getpass
+import pyodbc
+import pandas as pd
+import matplotlib.pyplot as plt
 
+from datetime import datetime
+from pycaret.classification import *
+from data_sci_toolkit.aws_tools import permission_tools
 
 
 
