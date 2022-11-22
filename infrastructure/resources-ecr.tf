f1ea5c92a1305247299fd57cf1b6edfaed47bd29
PMorrison2 <pmorrison@stellaralgo.com>
2022-11-22 08:57:04 -0700

    Lambda - Finish terraform for PP

--- infrastructure/resources-ecr.tf
+++ infrastructure/resources-ecr.tf
@@ -6,3 +6,12 @@ module "dw_ecr_repository_retention" {
   scan_on_push    = true
   tags            = local.tags
 }
+
+module "dw_ecr_repository_product_propensity" {
+  count = var.deploy_retention || var.deploy_product_propensity ? 1 : 0
+
+  source          = "../../infrastructure-common/modules/ecr_repository"
+  repository_name = "${local.model_name}-product-propensity"
+  scan_on_push    = true
+  tags            = local.tags
+}
