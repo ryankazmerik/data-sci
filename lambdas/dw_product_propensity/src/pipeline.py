a19ac75322b5f7eb1e813571f353599d40d6051b
PMorrison2 <pmorrison@stellaralgo.com>
2022-11-30 10:46:23 -0700

    Lambda - Clean dw_pp

--- lambdas/dw_product_propensity/src/pipeline.py
+++ lambdas/dw_product_propensity/src/pipeline.py
@@ -100,7 +100,6 @@ def run(event, context):
     best_model = create_model(model_matrix)
     final_model = finalize_model(best_model)
 
-    print(f"Season Year Values: {df['seasonYear'].value_counts()}")
     df_inference = df.loc[df["seasonYear"].astype("int") >= 2023]
     df_inference = df_inference.fillna(0)
 
