fbaca45bbb831f1b49c7ee0ab964bc98450e6a6b
Ryan Kazmerik <rkazmerik@stellaralgo.com>
2023-01-27 16:18:45 -0700

    Cleaning up

--- shared_utilities/shared_utilities/helpers.py
+++ shared_utilities/shared_utilities/helpers.py
@@ -1,35 +1,7 @@
 import botocore
 import boto3
-import json
-import pandas as pd
 import psycopg2
 import subprocess
-
-from sqlalchemy import true
-
-
-def hello_world():
-    
-    return "Hello World"
-
-
-def get_aws_session(profile, retry = True):
-    
-    session = boto3.Session(profile_name=profile)
-    sts = session.client('sts')
-    
-    try:
-        identity = sts.get_caller_identity()
-        print(f"Authorized as {identity['UserId']}")
-        return session
-    
-    except botocore.exceptions.UnauthorizedSSOTokenError:
-        if retry:
-            subprocess.run(['aws','sso', 'login', '--profile', profile])
-            return get_aws_session(profile, False)
-    
-        else:
-            raise
 
 
 def get_redshift_connection(cluster: str, database: str) -> psycopg2._psycopg.connection:
@@ -64,101 +36,6 @@ def get_redshift_connection(cluster: str, database: str) -> psycopg2._psycopg.co
     return cnxn
 
 
-def get_retention_dataset(cluster:str, database:str, lkupclientid:str, start_year:int, end_year:int) -> pd.DataFrame:
-
-    conn = get_redshift_connection(cluster, database)
-
-    cur = conn.cursor()
-    cur.execute(f"CALL ds.getretentionmodeldata({lkupclientid}, {start_year}, {end_year}, 'rkcursor')")
-
-    named_cursor = conn.cursor('rkcursor')
-    data = named_cursor.fetchall()
-
-    cols = [row[0] for row in named_cursor.description]
-    df = pd.DataFrame(data=data, columns=cols)
-
-    conn.commit()
-
-    return df
-
-
-def get_product_propensity_dataset(cluster:str, database:str, lkupclientid:str, scoring_year:int, type_flag:int) -> pd.DataFrame:
-
-    conn = get_redshift_connection(cluster, database)
-
-    cur = conn.cursor()
-    cur.execute(f"CALL ds.getproductpropensitymodeldata({lkupclientid}, {scoring_year}, {type_flag}, 'rkcursor')")
-
-    named_cursor = conn.cursor('rkcursor')
-    data = named_cursor.fetchall()
-
-    cols = [row[0] for row in named_cursor.description]
-    df = pd.DataFrame(data=data, columns=cols)
-
-    conn.commit()
-
-    return df
-
-
-def get_event_propensity_training_dataset(cluster:str, database:str, lkupclientid:str, start_year:int, end_year:int) -> pd.DataFrame:
-
-    conn = get_redshift_connection(cluster, database)
-
-    cur = conn.cursor()
-    cur.execute(f"CALL ds.geteventpropensitytrainingdata({lkupclientid}, {start_year}, {end_year}, 'rkcursor')")
-
-    named_cursor = conn.cursor('rkcursor')
-    data = named_cursor.fetchall()
-
-    cols = [row[0] for row in named_cursor.description]
-    df = pd.DataFrame(data=data, columns=cols)
-
-    conn.commit()
-
-    return df
-
-
-def get_event_propensity_scoring_dataset(cluster:str, database:str, lkupclientid:str, game_date:str) -> pd.DataFrame:
-
-    conn = get_redshift_connection(cluster, database)
-
-    cur = conn.cursor()
-    cur.execute(f"CALL ds.geteventpropensityscoringdata({lkupclientid}, cast('{game_date}' as date), 'rkcursor')")
-
-    named_cursor = conn.cursor('rkcursor')
-    data = named_cursor.fetchall()
-
-    cols = [row[0] for row in named_cursor.description]
-    df = pd.DataFrame(data=data, columns=cols)
-
-    conn.commit()
-
-    return df
-
-
-def get_event_propensity_gamedays(cluster:str, database:str, lkupclientid:str) -> pd.DataFrame:
-
-    conn = get_redshift_connection(cluster, database)
-
-    cur = conn.cursor()
-    cur.execute(f"""
-        SELECT DISTINCT SPLIT_PART(eventdate, ' ', 1) as game_date
-        FROM {database}.dw.cohortpurchase 
-        WHERE lkupclientid = {lkupclientid}
-        AND eventdate > CURRENT_DATE 
-        AND productgrouping = 'Full Season'
-        ORDER BY game_date
-    """)
-
-    data = cur.fetchall()
-
-    cols = [row[0] for row in cur.description]
-    df = pd.DataFrame(data=data, columns=cols)
-
-    conn.commit()
-
-    return df
-
 def establish_aws_session(profile: str, retry = True) -> boto3.Session:
     """Creates a session with the desired profile without requiring manual console input
 
@@ -185,6 +62,7 @@ def establish_aws_session(profile: str, retry = True) -> boto3.Session:
     
         else:
             raise
+
 
 def get_s3_bucket_items(session: boto3.Session, bucket: str, prefix: str) -> list:
     """ Recursively gets all items from a bucket with specified prefix. Normal limit is 1000, this fetches the full list.
